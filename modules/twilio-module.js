d25f2fceb239012c69d8f4fd4c7ed332e6f72311
niravshah <nirav.shah83@gmail.com>
2015-12-11 12:21:55 +0000

    Twilio Integration

--- modules/twilio-module.js
+++ modules/twilio-module.js
@@ -1,24 +1,32 @@
 var exports = module.exports = {};
 var twilio = require('twilio')('AC83e7e43fe1b509ba02108e6b35256f63', 'f648ed4e8609d9d594355e75cd981f4c');
+var Chance = require('chance');
+var chance = new Chance();
+var twilio_number = '441202835085';
+var redis = require("redis");
+var rediscli = redis.createClient();
 
-
-exports.sendVerificationCode = function(toNumber, fromNumber, code, rC) {
-//Send an SMS text message
-twilio.sendMessage({
-    to:toNumber,
-    from: fromNumber', // A number you bought from Twilio and can use for outbound communication
-    body: code
-
-}, function(err, responseData) { //this function is executed when a response is received from Twilio
-    if (!err) { // "err" is an error received during the request, if any
-        // "responseData" is a JavaScript object containing data received from Twilio.
-        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-        console.log(responseData.from); // outputs "+14506667788"
-        console.log(responseData.body); // outputs "word to your mother."
-
-    }
-});
-
-
+exports.sendVerificationCode = function(userid, toNumber, rC) {    
+    var code = chance.natural({min: 1000, max: 9999});     
+    twilio.sendMessage({
+        to:toNumber,
+        from: twilio_number,
+        body: code
+    }, function(err, responseData) {
+        if (!err) {
+            console.log('Error:', responseData.from);
+            console.log('Error:', responseData.body);
+            var key = 'twilio:' + userid
+            rediscli.set(key, code,function(err){                
+                if(err){
+                    console.log('Errr!');
+                }
+            });            
+            rC(responseData)    
+        }else{           
+            rC(err)
+        }        
+        
+    });
 }
+
--- routes/twilio-routes.js
+++ routes/twilio-routes.js
@@ -2,8 +2,13 @@ var twi = require('../modules/twilio-module.js')
 
 
 module.exports = function(app) {    
-    app.get('/api/code/:to', function(req, res){       
-        twi.sendVerificationCode(req.params.to,'441202835085','1234', function(result){res.status(200).send(result)})
-    });        
+    app.get('/api/code/:user/:phone', function(req, res){       
+        twi.sendVerificationCode(req.params.user,req.params.phone, function(result){res.status(200).send(result)})
+    });            
+    
+    app.get('/api/code/verify/:user/:code', function(req, res){       
+        twi.verifyCode(req.params.user,req.params.code,'441202835085','1234', function(result){res.status(200).send(result)})
+    });            
+
     
 }
